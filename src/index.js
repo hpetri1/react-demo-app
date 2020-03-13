817ef0903f31a2b95bc771a0b8f1b6e181279ba5
hpetri1 <39070807+hpetri1@users.noreply.github.com>
2020-03-13 01:01:27 -0600

    Update index.js
    Added first Message component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Message extends React.Component {
+    render() {
+        return (
+            <div style={style}>
+                <h1 id="heading-element">This is my React demo app</h1>
+                <p>Check this out</p>
+            </div>
+        )
+    }
+}
+
 var style = {
     backgroundColor: 'orange',
     color: 'white',
@@ -13,10 +24,7 @@ var style = {
 //     'This is my React demo app'
 // )
 
-ReactDOM.render(
-    <div>
-        <h1>This is my React demo app</h1>
-        <p>Chick this out</p>
-    </div>,
+ReactDOM.render( 
+    <Message />,
     document.getElementById('root')
 )
