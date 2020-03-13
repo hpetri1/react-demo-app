7fa0b198ae2f354c10fcc033f4da0a6fe28f36eb
hpetri1 <39070807+hpetri1@users.noreply.github.com>
2020-03-13 16:41:09 -0600

    Update index.js
    Added a new MoneyRaised component as a function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component} from 'react';
+import {render} from 'react-dom';
 
-class Message extends React.Component {
+let moneyRaised = {
+    total: 25000,
+    daily: 750,
+    goal: 55000
+}
+
+class Message extends Component {
     render() {
         return (
             <div style={style}>
@@ -18,7 +24,40 @@ var style = {
     fontFamily: 'Arial'
 }
 
-ReactDOM.render( 
-    <Message color="purple" textheading="This is my React demo app" percent={100} />,
+const getPercent = decimal => {
+    return decimal * 100 + '%'
+}
+
+const calcGoalProgress = (total, goal) => {
+    return getPercent(total/goal)
+}
+
+const MoneyRaised = ({total, daily, goal}) => {
+    return (
+        <section>
+                <div>
+                    <h2>Money Raised</h2>
+                </div>
+                <div>
+                    <p>To date: ${total}</p>
+                </div>
+                <div>
+                    <p>Daily amount: ${daily}</p>
+                </div>
+                <div>
+                    <p>Goal: ${goal}</p>
+                </div>
+                <div>
+                    <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
+                </div>
+            </section>
+    )
+}
+
+render( 
+    <div>
+        <Message color="purple" textheading="This is my React demo app" percent={100} />
+        <MoneyRaised total={moneyRaised.total} daily={moneyRaised.daily} goal={moneyRaised.goal}/>
+    </div>,
     document.getElementById('root')
 )
